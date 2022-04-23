40776700fe19f234158750ac7f4eb6394870d858
roman4i <verb.r17@gmail.com>
2022-04-23 22:24:03 +0300

    Added head block (title and description)

---
+++ src/Base/Head/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Head = () => {
+    return (
+        <>
+            <div className="mainTitle">
+                Генератор причин російської агресії
+            </div>
+            <div className="subText">
+            Застосунок автоматично генерує причину, випадковим чином, застосовуючи генератор чисел
+            </div>
+        </>
+    );
+}
+
+export default Head;
